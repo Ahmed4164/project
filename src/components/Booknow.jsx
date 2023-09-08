2e30ecac5a74e7a45bd24d1842a946efb17b5bdc
Mohammed Ahmed <72145684+Ahmed4164@users.noreply.github.com>
2023-09-08 14:30:07 +0530

    Update Booknow.jsx

--- src/components/Booknow.jsx
+++ src/components/Booknow.jsx
@@ -4,41 +4,14 @@ import Buttons from "./Buttons";
 import Img from "../assets/taskweb/Group 167.svg";
 import InputWrapper from "./InputWrapper";
 import Counter from "./Counter";
-// import Buttons from "./Buttons";
-// const Section = styled.div`
-//   background-color: red;
-//   height: 100vh;
-//   scroll-snap-align: center;
-// `;
+
 const Booknow = () => {
   return (
     <Section>
       <ContainerLeft>
-        {/* <Container> */}
-
         <img src={Img} alt="" srcset="" />
-        {/* </Container> */}
       </ContainerLeft>
       <ContainerRight>
-        {/* <div>
-          <p>Tropical Adventure</p>
-        </div>
-        <div>
-          <p>for Students.</p>
-        </div>
-        <div className="quote">
-          <h4>Student Tropical Vacation: Relax and Recharge</h4>
-        </div>
-        <div className="para">
-          <li>Lorem ipsum dolor, sit amet</li>
-          <li>Lorem ipsum dolor, sit amet</li>
-          <li>Lorem ipsum dolor, sit amet</li>
-          <li>Lorem ipsum dolor, sit amet</li>
-          <li>Lorem ipsum dolor, sit amet</li>
-        </div>
-        <div className="buttonContainer">
-          <Buttons name="Explore More" />
-        </div> */}
         <div className="calcouter">
           <div className="content">
             <h4>Book now</h4>
@@ -64,7 +37,6 @@ const Booknow = () => {
             </div>
           </div>
           <div className="button">
-            {/* <Buttons name="Book now" /> */}
             <button class="button-31" role="button">
               Book Now
             </button>
@@ -76,18 +48,13 @@ const Booknow = () => {
 };
 const Section = styled.div`
   background-color: #ffffff;
-  /* height: 100vh; */
   scroll-snap-align: center;
   display: flex;
   justify-content: center;
-  /* border: 2px solid red; */
   padding-inline: 6% ;
-  /* @media (max-width: 768px) {
-    flex-direction: column-reverse;
-  } */
-  /* align-items: center; */
+
   @media (max-width: 992px) {
-      /* border: 2px solid red; */
+      
     }
     @media (max-width: 840px) {
       flex-direction: column-reverse;
@@ -98,30 +65,17 @@ const Section = styled.div`
 const ContainerLeft = styled.div`
   width: 40vw;
   height: 65rem;
-  /* border: 2px solid red; */
   display: flex;
   justify-content: center;
   align-items: center;
   flex-direction: column;
-  /* padding-inline: -10rem; */
-  /* padding: 10rem; */
-  /* @media (max-width: 768px) {
-      display: block;
-      width: 100%;
-      margin: auto;
-    } */
     @media (max-width: 992px) {
-      /* border: 2px solid red; */
+      
     }
   
   img {
     height: 100%;
-    /* border: 2px solid red; */
     margin-bottom: 11rem;
-    /* @media (max-width: 768px) {
-      margin-bottom: 0;
-    } */
-    /* height: 58rem; */
     
     @media (max-width: 768px) {
     width: 33rem;
@@ -132,27 +86,18 @@ const ContainerLeft = styled.div`
   
 `;
 const ContainerRight = styled.div`
-  /* width: 63rem; */
-  /* height: 52rem; */
   padding-inline-start: 15rem;
-  /* border: 2px solid red; */
-  /* position: relative; */
   display: flex;
   justify-content: center;
-  /* align-items: center; */
-  /* padding-top: 12rem ; */
   flex-direction: column;
   @media (max-width: 992px) {
-      /* border: 2px solid red; */
       padding-inline-start: 0;
     }
   .calcouter {
-    /* border: 2px solid blue; */
     border-radius: 2rem;
     width: 42rem;
     height: 60rem;
     padding: 3rem;
-    /* position: relative; */
     box-shadow: rgba(0, 0, 0, 0.2) 0rem 1.8rem 5rem -1rem;
     .content {
       padding-bottom: 3rem;
@@ -177,22 +122,16 @@ const ContainerRight = styled.div`
     font-weight: 400;
   }
   .boxes {
-    /* border: 2px solid red; */
     margin-bottom: 2rem;
     display: grid;
     grid-template-rows: repeat(3, 1fr);
     grid-template-columns: 1fr 1fr;
     height: 34rem;
-    /* grid-auto-rows: 100px; */
-    /* grid-row-gap: 2rem; */
-    /* grid-column-gap: 2rem; */
     grid-gap: 2rem;
   }
   .subboxes {
-    /* border: 1px solid green; */
   }
   .box1 {
-    /* background-color: red; */
     grid-row: 1/2;
     grid-column: 1/3;
   }
@@ -221,7 +160,6 @@ const ContainerRight = styled.div`
     user-select: none;
     -webkit-user-select: none;
     touch-action: manipulation;
-    /* width: 100%; */
     font-family: Roboto;
     font-size: 1.5rem;
     font-weight: 700;
@@ -229,9 +167,6 @@ const ContainerRight = styled.div`
     letter-spacing: .12607004642486572rem;
     text-align: center;
     position: absolute;
-    /* bottom: ; */
-    /* width: 88px; */
-    /* height: 18px */
   }
 
   .button-31:hover,
